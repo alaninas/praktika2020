80d2952b79001ba8dac2474b80986166513c7ad8
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-09-04 22:19:08 +0300

    Cleaning code

--- user.js
+++ user.js
@@ -5,7 +5,6 @@ class User {
         this.password = password;
         this.email = email;
         this.info = info;
-        // this.friends = new Array("Saab", "Volvo", "BMW");
         this.friends = []
     }
 
@@ -65,52 +64,13 @@ class User {
     }
 
     addFriend(friend) {
-        // const numbers = {
-            // one: 1,
-        //   };
-        //   const objectArray = Object.entries(numbers);
-        //   objectArray.forEach(([key, value]) => {
-            // console.log(typeof key); // 'one'
-            // console.log(typeof value); // 1
-        //   });  
-
-          const objectArray = Object.entries(this.friends);
-        //   console.log(typeof objectArray);
-          objectArray.forEach(([key, value]) => {
-            // console.log(typeof key); // 'one'
-            // console.log(value); // 'one'
-            // console.log(typeof value); // 1
-          });        
-        // console.log(typeof this.name);
-
-        // console.log(typeof this.friends);
-        // let x = Object.values(this);
-        // console.log(typeof Object.values(this));
-        // console.log(typeof Object.values(x));
-        // console.log(typeof Object.keys(this.friends));
-
-        const numbers = {
-            one: 1,
-            two: 2,
-          };
-          
-        // console.log(typeof this.friends);
-        // console.log(typeof this.friends[0]);
-        // console.log(typeof this.name);
-        // console.log(typeof friend.name);
-
-        // console.log(JSON.stringify(this.friends));
-        // if (!this.friends.find(fr => fr.name == friend.name)) {
         if (this.friends.indexOf(friend.name) <= -1) {
-            //In the array!
-            // console.log(typeof this.name);
-            // console.log(JSON.stringify(this.friends));
-            // console.log(typeof friend.name);
+            // Not in the array
             this.friends.push(friend.name);
-            // if (friend.addFriend) {
             if (friend.friends.indexOf(this.name) <= -1) {
-                // friend.addFriend(this);
-                friend.friends.push(this.name);
+                friend.addFriend(this);
+                // Also works
+                // friend.friends.push(this.name);
             }
         } else {
             return;
