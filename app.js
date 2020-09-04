b843016fb61afcb7ea8143948eedfe7520af81fb
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-09-04 13:20:17 +0300

    Testing new functionality

--- app.js
+++ app.js
@@ -8,18 +8,20 @@ const callbackFn = () => {
 const firstUser = new User('Petras');
 firstUser.addAge(55);
 
-const secondUser = new User('PDraugas1','PDraugas1Pswd');
+const secondUser = new User('PD1','PD1Pswd');
 secondUser.addAge(5);
 
 firstUser.createPassword('PetrasPswd','PetrasPswd');
 
 secondUser.createPassword('PD1','PD2');
 
-const thirdUser = new User('PDraugas2','PD2Pswd', 'PD2email.com');
+const thirdUser = new User('PD2','PD2Pswd', 'PD2email.com');
 thirdUser.addAge(52);
 
 thirdUser.addEmail('PD2newEmail.com');
 firstUser.addEmail('PetrasEmail.com');
+
+thirdUser.changeEmail('PD2newEmail.com');
 
 fs.writeFile('users.txt', 
              JSON.stringify([firstUser,secondUser, thirdUser]), 
