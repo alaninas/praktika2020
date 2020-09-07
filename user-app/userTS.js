037ffaad9994edffc5fbf94d2fde015083d193a7
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-09-07 15:06:20 +0300

    Adding full User functionality

--- user-app/userTS.js
+++ user-app/userTS.js
@@ -8,6 +8,72 @@ var User = /** @class */ (function () {
         this.email = email;
         this.friends = friends;
     }
+    User.prototype.addAge = function (age) {
+        this.age = age;
+    };
+    User.prototype.createPassword = function (pwd1, pwd2) {
+        if (pwd1 === pwd2) {
+            this.password = pwd1;
+        }
+    };
+    User.prototype.addEmail = function (email) {
+        if (this.email === undefined) {
+            this.email = email;
+        }
+    };
+    User.prototype.changeEmail = function (email) {
+        if (this.email !== email) {
+            this.email = email;
+        }
+    };
+    // Answer:
+    // https://stackoverflow.com/questions/11796093/is-there-a-way-to-provide-named-parameters-in-a-function-call-in-javascript
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    User.prototype.addInfo = function (_a) {
+        var _b = _a === void 0 ? { age: undefined, height: undefined, physAddress: '' } : _a, age = _b.age, height = _b.height, physAddress = _b.physAddress;
+        if (!this.age) {
+            this.age = age;
+        }
+        if (!this.height) {
+            this.height = height;
+        }
+        if (!this.physAddress) {
+            this.physAddress = physAddress;
+        }
+    };
+    User.prototype.changeInfo = function (_a) {
+        var _b = _a === void 0 ? { age: undefined, height: undefined, physAddress: '' } : _a, age = _b.age, height = _b.height, physAddress = _b.physAddress;
+        if (this.age !== age) {
+            this.age = age;
+        }
+        if (this.height !== height) {
+            this.height = height;
+        }
+        if (this.physAddress !== physAddress) {
+            this.physAddress = physAddress;
+        }
+    };
+    User.prototype.addFriend = function (friend) {
+        if (this.friends.indexOf(friend.name) <= -1) {
+            // Not in the array
+            this.friends.push(friend.name);
+            if (friend.friends.indexOf(this.name) <= -1) {
+                friend.addFriend(this);
+                // Also works
+                // friend.friends.push(this.name);
+            }
+        }
+    };
+    User.prototype.removeFriend = function (friend) {
+        if (this.friends.indexOf(friend.name) > -1) {
+            // In the array
+            var index = this.friends.indexOf(friend.name);
+            this.friends.splice(index, 1);
+            if (friend.friends.indexOf(this.name) > -1) {
+                friend.removeFriend(this);
+            }
+        }
+    };
     return User;
 }());
 exports["default"] = User;
--- user-app/userTS.ts
+++ user-app/userTS.ts
@@ -4,12 +4,87 @@ export default class User {
     password:string; 
     email:string; 
     friends:Array<string>; 
+    age:number;
+    height:number;
+    physAddress:string;
  
     //constructor 
     constructor(name:string, password:string, email:string, friends:Array<string>) {
         this.name = name;
         this.password = password;
         this.email = email;
-        this.friends = friends
-    }    
+        this.friends = friends;
+    }  
+    
+    addAge(age:number) {
+        this.age = age;
+    }
+
+    createPassword(pwd1:string, pwd2:string) {
+        if (pwd1 === pwd2) {
+            this.password = pwd1;
+        }
+    }
+
+    addEmail(email:string) {
+        if (this.email === undefined) {
+            this.email = email;
+        }
+    }
+
+    changeEmail(email:string) {
+        if (this.email !== email) {
+            this.email = email;
+        }
+    }
+
+    // Answer:
+    // https://stackoverflow.com/questions/11796093/is-there-a-way-to-provide-named-parameters-in-a-function-call-in-javascript
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    addInfo({age, height, physAddress}={age:undefined, height:undefined, physAddress:''}) {
+        if (!this.age) {
+            this.age = age;
+        }
+        if (!this.height) {
+            this.height = height;
+        }
+        if (!this.physAddress) {
+            this.physAddress = physAddress;
+        }
+    }
+
+    changeInfo({age, height, physAddress}={age:undefined, height:undefined, physAddress:''}) {
+        if (this.age !== age) {
+            this.age = age;
+        }
+        if (this.height !== height) {
+            this.height = height;
+        }
+        if (this.physAddress !== physAddress) {
+            this.physAddress = physAddress;
+        }
+    }
+
+    addFriend(friend:User) {
+        if (this.friends.indexOf(friend.name) <= -1) {
+            // Not in the array
+            this.friends.push(friend.name);
+            if (friend.friends.indexOf(this.name) <= -1) {
+                friend.addFriend(this);
+                // Also works
+                // friend.friends.push(this.name);
+            }
+        }
+    }
+
+    removeFriend(friend:User) {
+        if (this.friends.indexOf(friend.name) > -1) {
+            // In the array
+            let index = this.friends.indexOf(friend.name);
+            this.friends.splice(index, 1);
+            if (friend.friends.indexOf(this.name) > -1) {
+                friend.removeFriend(this);
+            }
+        }
+    }
 }
