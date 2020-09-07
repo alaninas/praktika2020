1172f014a6ac0ef4ee22c9557721e8af8dd582e5
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-09-07 15:32:45 +0300

    Add appTS in full

--- user-app/appTS.ts
+++ user-app/appTS.ts
@@ -1,5 +1,49 @@
 import User from './userTS';
+import * as fs from 'fs';
 
-const U1 = new User('U1','','',['F1','F2']);
+const callbackFn = () => {
+    console.log("success");
+}
 
-console.log(JSON.stringify(U1));
+const firstUser = new User('Petras','','',[]);
+firstUser.addAge(55);
+
+const secondUser = new User('PD1','PD1Pswd','',[]);
+secondUser.addAge(5);
+
+firstUser.createPassword('PetrasPswd','PetrasPswd');
+
+secondUser.createPassword('PD1', 'PD2');
+
+const thirdUser = new User('PD2', 'PD2Pswd', 'PD2email.com',[]);
+thirdUser.addAge(52);
+
+thirdUser.addEmail('PD2newEmail.com');
+firstUser.addEmail('PetrasEmail.com');
+
+thirdUser.changeEmail('PD2newEmail.com');
+// thirdUser.addInfo('PD2NewInfo');
+// thirdUser.changeInfo('PD2NewInfo');
+
+thirdUser.addInfo({ age: undefined, height : 175, physAddress: '' });
+thirdUser.addInfo({ age: undefined, height : 180, physAddress: '' });
+thirdUser.changeInfo({ age: undefined, height : 177, physAddress: '' });
+
+thirdUser.addFriend(firstUser);
+thirdUser.addFriend(firstUser);
+thirdUser.addFriend(firstUser);
+
+firstUser.removeFriend(thirdUser);
+firstUser.removeFriend(thirdUser);
+
+firstUser.addFriend(secondUser);
+
+fs.writeFile('users.json', 
+        JSON.stringify([firstUser, secondUser, thirdUser]), 
+        {}, 
+        callbackFn);
+
+// fs.writeFile('users.txt', 
+              // JSON.stringify([thirdUser, firstUser]), 
+              // {}, 
+              // callbackFn);
