a398cfc7e731fbed2954d8faca52e6fd549c0ce4
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-09-16 02:56:08 +0300

    Add User delete

--- user-app/src/startMongo.ts
+++ user-app/src/startMongo.ts
@@ -5,6 +5,7 @@ import mongoose, { Document, model, Schema } from 'mongoose';
 import IPerson from './models/user.interface'
 import PersonSchema from './models/user.schema';
 
+
 mongoose.connect('mongodb://localhost:27017/users', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     // tslint:disable-next-line: no-console
     return console.log('Success');
@@ -12,13 +13,15 @@ mongoose.connect('mongodb://localhost:27017/users', {useNewUrlParser: true, useU
 }, error => console.log(error));
 
 const UserModel = model<IPerson>('Person', PersonSchema);
+
+
+// Sample runners
 const testPerson = UserModel.create({ name: 'petras', age: 22 }).then(() => {
     // tslint:disable-next-line: no-console
     return console.log('Successful creation!');
 // tslint:disable-next-line: no-console
 }, error => console.log(error))
-
-UserModel.findById('5f60b756e683317d7f192516', (err: any, response: IPerson) => {
+UserModel.findById('5f60b756e683317d7f192516', (err: any, response: IPerson | null) => {
     if (response) {
         // tslint:disable-next-line: no-console
         console.log('---> GOT:' + response);
@@ -30,13 +33,14 @@ UserModel.findById('5f60b756e683317d7f192516', (err: any, response: IPerson) => 
 // UserModel.remove({ _id: this.toObjectId('5f60b756e683317d7f192516') }, (err) => callback(err, null));
 
 
+// Setting endpoints
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 // get all users
 app.get('/users', (req, res) => {
-    UserModel.find({}, (err: any, result: IPerson[]) => {
+    UserModel.find({}, (err: any, result: IPerson[] | null) => {
         if (result) {
             res.json(result);
         } else {
@@ -48,7 +52,7 @@ app.get('/users', (req, res) => {
 // get single user
 // ?
 app.get('/users/:id/', (req, res) => {
-    UserModel.findById(req.params.id, (err: any, result: IPerson) => {
+    UserModel.findById(req.params.id, (err: any, result: IPerson | null) => {
         if (result) {
             res.json(result);
         } else {
@@ -61,7 +65,7 @@ app.get('/users/:id/', (req, res) => {
 app.post('/users/', (req, res) => {
     const uname = req.body.name;
     if (uname) {
-        UserModel.create({ name: uname }, (err: any, result: IPerson) => {
+        UserModel.create({ name: uname }, (err: any, result: IPerson | null) => {
             if (err) {
                 res.status(400).send('Error in inserting user');
             } else {
@@ -77,8 +81,20 @@ app.put('/users/', (req, res) => {
     res.send('update one or more users');
 })
 
+// delete one or more users
 app.delete('/users/', (req, res) => {
-    res.send('delete one or more users');
+    const uid = req.body.id;
+    if (uid) {
+        UserModel.findByIdAndDelete(uid, (err: any, result: IPerson | null) => {
+            if (err) {
+                res.status(404).send('No such user found');
+            } else {
+                res.json(result);
+            }
+        });
+    } else {
+        res.status(400).send('No user ID provided');
+    }
 })
 
 app.listen(3030);
