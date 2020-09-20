8dd311b0003e8689a3b296c45020b0a228a40f46
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-09-20 15:07:41 +0300

    Clean conditionals

--- user-app/src/routes/users.route.ts
+++ user-app/src/routes/users.route.ts
@@ -12,32 +12,13 @@ const UsersRouter = express.Router();
 UsersRouter.get('/users', (req, res) => {
     UserModel.find({}, (err: any, result: IPerson[]) => {
         // if return value := [], empty array === TRUE; have to compare to an array length instead
-        result.length > 0 ? res.json(result) : res.status(404).json({err: 'DB is empty'});
+        result.length > 0 ? res.json(result) : res.status(404).json({Error: 'DB is empty'});
     })
 })
 
-// id:= 6f64a595fc86ad6952e2a47f
-// returns := null
-// why? if the object doesn't exist?
-// should return 404.
-// Fixed: App Restart is required.
 UsersRouter.get('/users/:id/', (req, res) => {
     UserModel.findById(req.params.id, (err: any, result: IPerson | null) => {
-        // result ? res.json(result) : res.status(404).json({err});
-        result ? res.json(result) : res.status(404).json({err: 'No user found'});;
-    })
-})
-
-UsersRouter.post('/users/login', (req, res) => {
-    const uid = req.body.id;
-    const upwd = md5(req.body.password);
-    if (!uid) return res.status(400).send('No user ID provided');
-    UserModel.findById(uid, (err: any, result: IPerson | null) => {
-        if (result) {
-            result.password === upwd ? res.send('Successful login') : res.status(400).send('Wrong password provided: ' + upwd + ', from DB: ' + result.password);
-        } else {
-            res.status(404).json({err: 'No such user in BD'});
-        }
+        result ? res.json(result) : res.status(404).json({Error: 'No user found'});
     })
 })
 
@@ -47,7 +28,7 @@ UsersRouter.post('/users/', (req, res) => {
     if (!uname || !upwd) return res.status(400).send('Insufficient information provided');
     const newUser = new UserModel({ name: uname, password: upwd });
     newUser.save((err: any, result: IPerson) => {
-        err ? res.json(result) : res.status(400).json({err: 'Error while saving the document'});
+        err ? res.json(result) : res.status(400).json({Error: 'Error while saving the document'});
     });
 })
 
@@ -57,16 +38,13 @@ UsersRouter.put('/users/', (req, res) => {
     const upwd = md5(data.password);
     if (!uid || !upwd) return res.status(400).send('Insufficient information provided');
     UserModel.findById(uid, (err: any, result: IPerson | null) => {
-        if (result) {
-            try {
-                const util = new UserVUtility(result);
-                const fields = {password: util.password(upwd), email: util.email(data.email), address: util.address(data.address), age: util.age(data.age), height: util.height(data.height)};
-                result.updateOne(fields, (err2: any, raw: any) => {err2 ? res.status(400).send(err2) : res.json(raw);});
-            } catch (error) {
-                res.status(400).json({error: error.message});
-            }
-        } else {
-            res.status(404).json({err});
+        if (!result) return res.status(404).json({Error: 'No such user in DB'});
+        try {
+            const util = new UserVUtility(result);
+            const fields = {password: util.password(upwd), email: util.email(data.email), address: util.address(data.address), age: util.age(data.age), height: util.height(data.height)};
+            result.updateOne(fields, (err2: any, raw: any) => {err2 ? res.status(400).send(err2) : res.json(raw);});
+        } catch (error) {
+            res.status(400).json({Error: error.message});
         }
     });
 })
@@ -80,6 +58,16 @@ UsersRouter.delete('/users/', (req, res) => {
     UserModel.findOneAndDelete({_id: uid}, (err: any, result: IPerson | null) => {
         result ? res.json(result) : res.status(404).json({err});
     });
+})
+
+UsersRouter.post('/users/login', (req, res) => {
+    const uid = req.body.id;
+    const upwd = md5(req.body.password);
+    if (!uid) return res.status(400).send('No user ID provided');
+    UserModel.findById(uid, (err: any, result: IPerson | null) => {
+        if (!result) return res.status(404).json({Error: 'No such user in DB'});
+        result.password === upwd ? res.send('Successful login') : res.status(400).send('Wrong password provided: ' + upwd + ', from DB: ' + result.password);
+    })
 })
 
 export default UsersRouter;
