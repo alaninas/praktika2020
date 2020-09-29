3a5e3f7764983aebae35817b79c6b5f07de152cd
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-09-29 09:10:52 +0300

    Add swagger CLI plugin

--- nestjs/user-app/src/main.ts
+++ nestjs/user-app/src/main.ts
@@ -1,12 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false }));
-
-  await app.listen(3000);
+    app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false }));
+  
+    const options = new DocumentBuilder()
+        .setTitle('User App')
+        .setDescription('API description')
+        .setVersion('1.0')
+        .addTag('userapp')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  
+    await app.listen(3000);
 }
 bootstrap();
