e2f51cc86b654be17dc5e1e5a2d0cce402388f58
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-10-06 10:49:51 +0300

    Add session notes

--- test-vue3/dummy-project/src/views/Users.vue
+++ test-vue3/dummy-project/src/views/Users.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="users">Users</div>
-  <div v-for="user in users" :key="user">
-    {{ user }}
+  <ul>
+      <li v-for="user in users.data" :key="user">
+        <!-- user component to output user data fields, with remove button -->
+        {{ user }}
+      </li>
+  </ul>
+  <div>
+    <input type="text" name="" id="" v-model="newUser"/>
+    <button @click="addUser(newUser)">add</button>
   </div>
-  <!-- <button @click="increment"> -->
-    <!-- Count is: {{ state.count }}, double is: {{ state.double }} -->
-  <!-- </button> -->
-  <!-- <button @click="clear"> -->
-    <!-- Clear counter -->
-  <!-- </button> -->
 </template>
 
 <script lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
 export default {
   setup () {
+    // name, age, email ..
+    // check if no empty user added
+    // css framework ? some...
     const users = reactive({
       data: [{ user: 'user1' }, { user: 'u2' }]
     })
 
-    // function clear () {
-    // state.count = 0
-    // }
+    const users2 = ref([])
+    // users2.value = [{ user: 'user1' }, { user: 'u2' }]
+
+    function addUser (newUser: string) {
+      const nu = { user: newUser }
+      users.data.push(nu)
+    }
 
     return {
-      users
+      users,
+      addUser
     }
   }
 }
