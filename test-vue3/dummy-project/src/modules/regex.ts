ae97f09822991838c960cc449a49ecd7da12aa98
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-10-19 04:38:11 +0300

    Fix: full string match regex

--- test-vue3/dummy-project/src/modules/regex.ts
+++ test-vue3/dummy-project/src/modules/regex.ts
@@ -2,4 +2,4 @@ export const zipcodeFirst = /^([\d+-?\d+]{4,6})\s*([A-Za-z- ]+(\s+[0-9]{1,3} g.)
 
 export const housenumberFirst = /^([0-9]{1,3})\s*([A-Za-z- ]+(\s+[0-9]{1,3} g.)?)?$/
 
-export const streetFirst = /^([A-Za-z- ]+(\s+[0-9]{1,3} g.)?|( g.)?)(\s+[0-9]{1,4})?$/
+export const streetFirst = /^([A-Za-z- ]+(\s+[0-9]{1,3} g.)?|( g.)?)(\s+[0-9]{1,4})?(\s+[A-Za-z]+)?(\s+[\d+-?\d+]{4,6})?$/
