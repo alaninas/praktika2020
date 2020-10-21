7b79aa9b2a364945cfc0edd6d68c2d7826341a8e
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-10-21 03:15:19 +0300

    Fix endless recursion on update

--- test-vue3/dummy-project/src/modules/directives/validate.ts
+++ test-vue3/dummy-project/src/modules/directives/validate.ts
@@ -1,21 +1,32 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 function updateErrorObject ({ el, vm }: { el: any; vm: any }) {
   vm.validationErrors = Object.assign({}, vm.validationErrors, {
     [el.name]: el.validationMessage
   })
 }
 
+function initRequiredError ({ el, vm }: { el: any; vm: any }) {
+  const index = Object.keys(vm.validationErrors).findIndex(key => key === el.name)
+  if (index < 0) {
+    // console.log(el.name)
+    // console.log(el.validationMessage)
+    updateErrorObject({ el, vm })
+  }
+}
+
 // See
 // https://logaretm.com/blog/2019-05-03-html-aided-vuejs-form-validation/
 // https://v3.vuejs.org/guide/migration/custom-directives.html#edge-case-accessing-the-component-instance
 export default {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   mounted (el: any, binding: any) {
     const vm = binding.instance
-    // Set messages in error object for required fields
-    if (el.validity.valueMissing) updateErrorObject({ el, vm })
+    if (el.validity.valueMissing) initRequiredError({ el, vm })
     el.addEventListener('input', () => {
       updateErrorObject({ el, vm })
     })
+  },
+  updated (el: any, binding: any) {
+    const vm = binding.instance
+    if (el.validity.valueMissing) initRequiredError({ el, vm })
   }
 }
