53744a04f94e214b3d7317a3938e9871feb89784
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-10-27 21:31:43 +0200

    Add age parseInt

--- nestMongoVue-user-app/user-app-frontend/src/modules/features/useUser.ts
+++ nestMongoVue-user-app/user-app-frontend/src/modules/features/useUser.ts
@@ -27,14 +27,13 @@ export function useUser () {
 
   function setUser (inputUser: UserInterface): UserInterface {
     const { street, houseNumber, city, zipCode } = { street: inputUser.street, houseNumber: inputUser.houseNumber?.toString(), city: inputUser.city, zipCode: inputUser.zipCode?.toString() }
-    user.value.name = inputUser.name
     user.value.password = inputUser.password
     user.value.passwordConfirm = inputUser.passwordConfirm
     user.value.email = inputUser.email
     user.value.firstname = inputUser.firstname
     user.value.lastname = inputUser.lastname
     user.value.country = inputUser.country
-    user.value.age = inputUser.age
+    if (inputUser.age) user.value.age = parseInt(inputUser.age.toString())
     setFullnameSring(user.value)
     if (street && houseNumber && city && zipCode) setUserAddress({ street, houseNumber, city, zipCode } as AddressInterface)
     return user.value
