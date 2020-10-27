c1fc88069f6c515749277ca250ba6d0d6bf36ee1
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-10-28 00:26:46 +0200

    Update routes

--- nestMongoVue-user-app/user-app-frontend/src/App.vue
+++ nestMongoVue-user-app/user-app-frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
-    <router-link to="/">Counter</router-link> |
-    <router-link to="/users">Users</router-link>
+    Users Home
+    <!-- <router-link to="/">Counter</router-link> |
+    <router-link to="/users">Users</router-link> -->
   </div>
   <router-view/>
 </template>
--- nestMongoVue-user-app/user-app-frontend/src/router/index.ts
+++ nestMongoVue-user-app/user-app-frontend/src/router/index.ts
@@ -1,14 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Counter from '../views/Counter.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Counter',
-    component: Counter
-  },
-  {
-    path: '/users',
     name: 'Users',
     component: () => import(/* webpackChunkName: "about" */ '../views/Users.vue')
   }
