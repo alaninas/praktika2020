fe6939e2059b3a20f1509fe819e40ee9c275bf96
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-01 17:48:25 +0200

    Add users table

--- nestMongoVue-user-app/user-app-frontend/src/views/Edit.vue
+++ nestMongoVue-user-app/user-app-frontend/src/views/Edit.vue
@@ -8,16 +8,27 @@
       </div>
     </div>
   </div>
+  <div class="card fluid users-saved"><p class="section">Users Saved</p>
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+      <Suspense>
+        <UsersTable />
+      </Suspense>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import EditUser from '@/components/forms/editUser/EditUser.vue'
+import UsersTable from '@/components/UsersTable.vue'
 import router from '@/router'
 
 export default {
   name: 'Edit',
   components: {
-    EditUser
+    EditUser,
+    UsersTable
   },
   setup () {
     function navigate () {
@@ -32,4 +43,8 @@ export default {
 div.card.fluid > p {
   font-variant-caps: all-small-caps;
 }
+// .users-saved table td,
+// .users-saved table th {
+//   color: #7d7d7d !important;
+// }
 </style>
