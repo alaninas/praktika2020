7d8a7b916c8a82ddfe8306641f6f17a7a1ba2622
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-02 03:33:13 +0200

    Update properties

--- nestMongoVue-user-app/user-app-frontend/src/modules/types/IUser.ts
+++ nestMongoVue-user-app/user-app-frontend/src/modules/types/IUser.ts
@@ -1,17 +1,17 @@
 export default interface UserInterface {
-    firstname?: string;
-    lastname?: string;
-    fullname?: string;
+    firstname?: string | undefined;
+    lastname?: string | undefined;
+    fullname?: string | undefined;
     age?: number;
     country?: string;
     url?: string;
     email?: string;
     password?: string;
     passwordConfirm?: string;
-    houseNumber?: number;
-    street?: string;
-    city?: string;
-    zipCode?: number;
+    houseNumber?: number | undefined;
+    street?: string | undefined;
+    city?: string | undefined;
+    zipCode?: number | undefined;
     address?: string;
     friends?: Array<string>;
     movies?: Array<string>;
