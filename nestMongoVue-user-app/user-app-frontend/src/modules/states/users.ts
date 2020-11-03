3aefb8e44fa2e04f69a4358547e15d498ec29399
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-03 09:26:59 +0200

    Update users logic

--- nestMongoVue-user-app/user-app-frontend/src/modules/states/users.ts
+++ nestMongoVue-user-app/user-app-frontend/src/modules/states/users.ts
@@ -35,37 +35,25 @@ async function getStateUser (id: string): Promise<UserInterface> {
 
 async function updateStateUser (newUser: UserInterface): Promise<Ref<UserInterface[]>> {
   await putUpdatedUser(newUser)
-  user.value = newUser
-  const allUsers = getState()
-  const index = allUsers.value.findIndex(el => el.email === newUser.email)
-  allUsers.value[index] = newUser
-  setState(allUsers.value)
+  const allUsers = await getAllUsers()
+  setState(allUsers.data)
   return getState()
 }
 
 async function addStateUser (newUser: UserInterface): Promise<Ref<UserInterface[]>> {
-  const response = await postNewUser(newUser)
-  const allUsers = getState()
-  allUsers.value.push(response.data)
-  setState(allUsers.value)
+  await postNewUser(newUser)
+  const allUsers = await getAllUsers()
+  setState(allUsers.data)
   return getState()
 }
 
 async function removeStateUser (userId: string): Promise<Ref<UserInterface[]>> {
-  const response = await deleteUser(userId)
-  const allUsers = getState()
-  const index = allUsers.value.findIndex(el => el.email === response.data.email)
-  allUsers.value.splice(index, 1)
-  setState(allUsers.value)
+  await deleteUser(userId)
+  const allUsers = await getAllUsers()
+  setState(allUsers.data)
   return getState()
 }
 
-// watch(users, (newUsers, oldUsers) => {
-//   history.push(newUsers)
-//   console.log('from users state watcher')
-//   console.log(newUsers)
-//   console.log(history.length)
-// })
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 watch([user, users], ([user, users], [prevUser, prevUsers]) => {
   history.push(users)
