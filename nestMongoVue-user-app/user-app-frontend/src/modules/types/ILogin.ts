6c0438bec2bae20afedaeee8ee118b4fdcbfb296
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-04 04:07:56 +0200

    Set id as optional

--- nestMongoVue-user-app/user-app-frontend/src/modules/types/ILogin.ts
+++ nestMongoVue-user-app/user-app-frontend/src/modules/types/ILogin.ts
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 export default interface LoginInterface {
   password: string;
   email: string;
-  _id: string;
+  _id?: string;
 }
 
-export const loginData = ref({ password: '', email: '', _id: '' } as LoginInterface)
+export const loginData = ref({ password: '', email: '' } as LoginInterface)
