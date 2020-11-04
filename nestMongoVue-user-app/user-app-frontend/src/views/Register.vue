a045eff6daa4395676679ca0762796934a7ee181
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-04 13:57:33 +0200

    Remove register users list

--- nestMongoVue-user-app/user-app-frontend/src/views/Register.vue
+++ nestMongoVue-user-app/user-app-frontend/src/views/Register.vue
@@ -8,25 +8,14 @@
       </div>
     </div>
   </div>
-  <div class="card fluid"><p class="section">Users Saved</p>
-    <div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12">
-      <Suspense>
-        <UsersTable />
-      </Suspense>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script lang="ts">
 import NewUser from '@/components/forms/newUser/NewUser.vue'
-import UsersTable from '@/components/UsersTable.vue'
 
 export default {
   components: {
-    NewUser,
-    UsersTable
+    NewUser
   },
   setup () {
     return { }
