1a7284ad5735db0db4379bd73fc93773f5751966
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-06 01:48:55 +0200

    Update loggedIn prompts

--- nestMongoVue-user-app/user-app-frontend/src/App.vue
+++ nestMongoVue-user-app/user-app-frontend/src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="nav" v-show="!isLoggedIn">
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/register">Register</router-link> |
-    <router-link to="/users">Users</router-link>
+  <div id="nav" class="row" v-show="!isLoggedIn">
+    <div class="col-lg-10 col-md-9 col-sm-7">
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link> |
+      <router-link to="/users">Users</router-link>
+    </div>
+    <div class="col-lg-2 col-md-3 col-sm-5">
+      <span>User: anonymous</span>
+    </div>
   </div>
-  <div id="nav" v-show="isLoggedIn">
-    <router-link to="/users">Users</router-link> |
-    <label role="button" class="button inverse responsive-padding responsive-margin" @click="logoutUser()">Auth Logout</label>
-  </div>
-  <div class="card fluid success" v-show="isLoggedIn">
-    <p>Logged in with email: {{ userLoginData.email }}</p>
-  </div>
-  <div class="card fluid warning" v-show="!isLoggedIn">
-    <p>Please login to edit profile</p>
+  <div id="nav" class="row" v-show="isLoggedIn">
+    <div class="col-lg-10 col-md-8 col-sm-6">
+      <router-link to="/users">Users</router-link>
+    </div>
+    <div class="col-lg-2 col-md-4 col-sm-6">
+      <router-link to="" @click="logoutUser()">Logout</router-link> |
+      <span>User: {{userLoginData.email}}</span>
+    </div>
   </div>
   <router-view/>
 </template>
