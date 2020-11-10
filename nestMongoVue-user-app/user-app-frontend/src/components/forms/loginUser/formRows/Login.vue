8b1d7751baafabe4aafc0c087606c98ef5ca7f38
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-10 16:07:00 +0200

    Set isPasswordForgotten flag on httpSuccess

--- nestMongoVue-user-app/user-app-frontend/src/components/forms/loginUser/formRows/Login.vue
+++ nestMongoVue-user-app/user-app-frontend/src/components/forms/loginUser/formRows/Login.vue
@@ -2,28 +2,43 @@
   <div class="row">
     <div class="col-lg-6 col-md-6 col-sm-12">
       <label for="emailInput">Email</label>
-      <input type="email" id="emailInput" name="email" :class="httpErrorMessage.pswdReset ? 'invalid' : ''" v-model="userLoginData.email" required v-validate />
+      <input
+        type="email"
+        id="emailInput"
+        name="email"
+        :class="httpErrors.email ? 'invalid' : ''"
+        v-model="userLoginData.email"
+        required
+        v-validate
+      />
       <div class="error">{{ validationErrors.email }}</div>
     </div>
     <div class="col-lg-6 col-md-6 col-sm-12">
       <label for="userPassword">Pswd1</label>
-      <input type="password" id="userPassword" name="password" v-model="userLoginData.password" v-validate />
+      <input
+        type="password"
+        id="userPassword"
+        name="password"
+        :class="httpErrors.email ? 'invalid' : ''"
+        v-model="userLoginData.password"
+        v-validate
+      />
       <div class="error">{{ validationErrors.password }}</div>
     </div>
   </div>
   <label
     v-show="!creds.isAuthenticated"
     role="button"
-    :class="isPasswordForgotten ? 'disabled button bordered' : 'responsive-padding responsive-margin tertiary'"
-    @click="resetUsersPassword(isPasswordForgotten, validationErrors)">
-      {{ isPasswordForgotten ? 'Check email for new password' : 'Forgot password ?' }}
+    :class="isPasswordReset ? 'disabled button bordered' : 'responsive-padding responsive-margin tertiary'"
+    @click="resetUsersPassword(isPasswordReset, validationErrors)">
+      {{ isPasswordReset ? 'Check email for new password' : 'Forgot password ?' }}
   </label>
-  <div class="error">{{ httpErrorMessage.pswdReset }}</div>
+  <div class="error">{{ httpErrors.email }}</div>
 </template>
 
 <script lang="ts">
 import validate from '@/directives/validate'
-import { validationErrors, httpErrorMessage } from '@/modules/states/formErrors'
+import { validationErrors, httpErrors } from '@/modules/states/formErrors'
 import { useLogin } from '@/modules/features/useLogin'
 import { ref } from 'vue'
 
@@ -38,22 +53,19 @@ export default {
     validate: validate
   },
   async setup (props: Readonly<{forgot: boolean} & {}>) {
-    const isPasswordForgotten = ref(props.forgot)
+    const isPasswordReset = ref(props.forgot)
     const { userLoginData, resetPassword, creds } = useLogin({})
 
-    function resetUsersPassword (param: boolean, valErrs: never[]) {
+    async function resetUsersPassword (isPasswordResetFlag: boolean, valErrs: never[]) {
       validationErrors.value = valErrs
-      const validationErrorsCount = Object.values(validationErrors.value).filter(el => !!el).length
-      console.log('--> sending logIn to server')
-      console.log(userLoginData.value.email)
-      console.log(validationErrors.value)
-      console.log(param)
-      if (!validationErrorsCount && param === false) {
-        resetPassword()
-        isPasswordForgotten.value = true
+      const valErrsCount = Object.values(validationErrors.value).filter(el => !!el).length
+      if (!valErrsCount && !isPasswordReset.value) {
+        isPasswordReset.value = await resetPassword(isPasswordResetFlag)
+        console.log('--> inside pswd reset')
+        console.log(isPasswordReset.value)
       }
     }
-    return { userLoginData, validationErrors, isPasswordForgotten, resetUsersPassword, creds, httpErrorMessage }
+    return { userLoginData, validationErrors, isPasswordReset, resetUsersPassword, creds, httpErrors }
   }
 }
 </script>
--- nestMongoVue-user-app/user-app-frontend/src/modules/features/useLogin.ts
+++ nestMongoVue-user-app/user-app-frontend/src/modules/features/useLogin.ts
@@ -1,6 +1,6 @@
 import { watch } from 'vue'
-import LoginInterface from '@/modules/types/ILogin'
-import { loadLoginData, loginStateUser, clearLoginState, authCredentials, performLogout, resetStatePassword } from '@/modules/states/login'
+import { LoginInterface } from '@/modules/types/ILogin'
+import { loadLoginData, loginStateUser, clearLoginState, authCredentials, performLogout, resetStatePassword, loginData } from '@/modules/states/login'
 
 export function useLogin ({ userLoginInit = { email: '', password: '', _id: '' } as LoginInterface, noDataReload = true }: { userLoginInit?: LoginInterface; noDataReload?: boolean }) {
   const userLoginData = loadLoginData(userLoginInit, noDataReload)
@@ -8,10 +8,6 @@ export function useLogin ({ userLoginInit = { email: '', password: '', _id: '' }
 
   function loginUser () {
     loginStateUser(userLoginData.value)
-  }
-
-  function resetPassword () {
-    resetStatePassword(userLoginData.value)
   }
 
   function logoutUser () {
@@ -22,10 +18,14 @@ export function useLogin ({ userLoginInit = { email: '', password: '', _id: '' }
     clearLoginState()
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  watch(creds, (creds) => {
+  async function resetPassword (isPasswordForgotten: boolean): Promise<boolean> {
+    return await resetStatePassword(userLoginData.value, isPasswordForgotten)
+  }
+
+  watch([creds, loginData], () => {
     console.log('---> check logging.... ')
-    console.log(creds)
+    console.log(creds.value)
+    console.log(userLoginData.value)
   })
 
   return { userLoginData, creds, loginUser, logoutUser, clearLoginData, resetPassword }
