30e51993810e2607652d170d5955af395d04bebd
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-12 21:24:26 +0200

    Move login buttons to the common row

--- nestMongoVue-user-app/user-app-frontend/src/components/forms/loginUser/LoginUser.vue
+++ nestMongoVue-user-app/user-app-frontend/src/components/forms/loginUser/LoginUser.vue
@@ -2,19 +2,30 @@
 <div>
   <form @submit.prevent="onSubmit(validationErrors)" onkeydown="return event.key != 'Enter';" id="userForm">
     <Suspense>
-      <Login v-bind:forgot="isPasswordForgotten" />
+      <Login />
     </Suspense>
-    <div v-show="!creds.isAuthenticated">
-      <input
-        type="submit"
-        value="Submit"
-        :class="userLoginData.password.length < 4 ? 'disabled button bordered' : 'button primary responsive-padding responsive-margin'"
-      />
+    <div class="row login-buttons">
+      <label
+        v-show="!creds.isAuthenticated"
+        role="button"
+        :class="isPasswordReset ? 'disabled button bordered' : 'responsive-padding responsive-margin bordered'"
+        @click="resetUsersPassword(isPasswordForgotten, validationErrors)">
+          {{ isPasswordForgotten ? 'Check email for new password' : 'Forgot password ?' }}
+      </label>
+      <label role="button" class="responsive-padding responsive-margin tertiary" @click="routerRedirect('Register')">
+        Register
+      </label>
+      <span v-show="!creds.isAuthenticated">
+        <input
+          type="submit"
+          value="Submit"
+          :class="userLoginData.password.length < 4 ? 'disabled button bordered responsive-padding responsive-margin' : 'button primary responsive-padding responsive-margin'"
+        />
+      </span>
     </div>
-    <div class="error">{{ httpErrors.password }}</div>
   </form>
   <div v-show="creds.isAuthenticated">
-    <label role="button" class="button primary" @click="routerRedirect('Users')">Next >></label>
+    <label role="button" class="responsive-padding responsive-margin primary" @click="routerRedirect('Users')">Next >></label>
   </div>
 </div>
 </template>
@@ -35,21 +46,26 @@ export default {
     validate: validate
   },
   async setup () {
-    const { userLoginData, loginUser, creds } = useLogin({ noDataReload: false })
+    const { userLoginData, loginUser, creds, resetPassword } = useLogin({ noDataReload: false })
     const isPasswordForgotten = ref(false)
 
     function onSubmit (valErrs: never[]) {
       validationErrors.value = valErrs
       const validationErrorsCount = Object.values(validationErrors.value).filter(el => !!el).length
-      console.log('--> sending logIn to server')
-      console.log(validationErrors.value)
-      console.log(userLoginData.value)
       if (!validationErrorsCount && userLoginData.value.password.length > 3) {
         loginUser()
+        isPasswordForgotten.value = false
+      }
+    }
+    async function resetUsersPassword (isPasswordResetFlag: boolean, valErrs: never[]) {
+      validationErrors.value = valErrs
+      const valErrsCount = Object.values(validationErrors.value).filter(el => !!el).length
+      if (!valErrsCount && !isPasswordForgotten.value) {
+        isPasswordForgotten.value = await resetPassword(isPasswordResetFlag)
       }
     }
 
-    return { onSubmit, validationErrors, creds, isPasswordForgotten, userLoginData, httpErrors, routerRedirect }
+    return { onSubmit, validationErrors, creds, isPasswordForgotten, userLoginData, httpErrors, routerRedirect, resetUsersPassword }
   }
 }
 </script>
--- nestMongoVue-user-app/user-app-frontend/src/components/forms/loginUser/formRows/Login.vue
+++ nestMongoVue-user-app/user-app-frontend/src/components/forms/loginUser/formRows/Login.vue
@@ -11,7 +11,7 @@
         required
         v-validate
       />
-      <div class="error">{{ validationErrors.email }}</div>
+      <div class="error">{{ validationErrors.email }} {{ httpErrors.email }}</div>
     </div>
     <div class="col-lg-6 col-md-6 col-sm-12">
       <label for="userPassword">Pswd1</label>
@@ -23,47 +23,24 @@
         v-model="userLoginData.password"
         v-validate
       />
-      <div class="error">{{ validationErrors.password }}</div>
+      <div class="error">{{ validationErrors.password }} {{ httpErrors.password  }}</div>
     </div>
   </div>
-  <label
-    v-show="!creds.isAuthenticated"
-    role="button"
-    :class="isPasswordReset ? 'disabled button bordered' : 'responsive-padding responsive-margin tertiary'"
-    @click="resetUsersPassword(isPasswordReset, validationErrors)">
-      {{ isPasswordReset ? 'Check email for new password' : 'Forgot password ?' }}
-  </label>
-  <div class="error">{{ httpErrors.email }}</div>
 </template>
 
 <script lang="ts">
 import validate from '@/directives/validate'
 import { validationErrors, httpErrors } from '@/modules/states/formErrors'
 import { useLogin } from '@/modules/features/useLogin'
-import { ref } from 'vue'
+import { routerRedirect } from '@/modules/utilities/router-utility'
 
 export default {
-  props: {
-    forgot: {
-      type: Boolean,
-      required: true
-    }
-  },
   directives: {
     validate: validate
   },
-  async setup (props: Readonly<{forgot: boolean} & {}>) {
-    const isPasswordReset = ref(props.forgot)
-    const { userLoginData, resetPassword, creds } = useLogin({})
-
-    async function resetUsersPassword (isPasswordResetFlag: boolean, valErrs: never[]) {
-      validationErrors.value = valErrs
-      const valErrsCount = Object.values(validationErrors.value).filter(el => !!el).length
-      if (!valErrsCount && !isPasswordReset.value) {
-        isPasswordReset.value = await resetPassword(isPasswordResetFlag)
-      }
-    }
-    return { userLoginData, validationErrors, isPasswordReset, resetUsersPassword, creds, httpErrors }
+  async setup () {
+    const { userLoginData } = useLogin({})
+    return { userLoginData, validationErrors, httpErrors, routerRedirect }
   }
 }
 </script>
