a7e24a866f537d514520e53a93b86fa58c1c3aff
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-12 19:40:11 +0200

    Add new property

--- nestMongoVue-user-app/user-app-frontend/src/components/forms/newUser/formRows/NewLogin.vue
+++ nestMongoVue-user-app/user-app-frontend/src/components/forms/newUser/formRows/NewLogin.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="row">
-    <div class="col-lg-4 col-md-12 col-sm-12">
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <label for="websiteInput">Website</label>
+      <input type="url" id="websiteInput" name="website" v-model="user.website" v-validate />
+      <div class="error">{{ validationErrors.website }}</div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
       <label for="emailInput">Email</label>
       <input type="email" id="emailInput" name="email" v-model="user.email" required v-validate />
       <div class="error">{{ validationErrors.email }}</div>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-12">
+    <div class="col-lg-3 col-md-6 col-sm-12">
       <label for="userPassword">Pswd1</label>
       <input type="password" id="userPassword" name="password" v-model="user.password" :class="userErrors.password ? 'invalid' : ''" minlength="4" required v-validate />
       <div class="error">{{ validationErrors.password }} {{ userErrors.password }}</div>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-12">
+    <div class="col-lg-3 col-md-6 col-sm-12">
       <label for="userPasswordConfirm">Pswd2</label>
       <input type="password" id="userPasswordConfirm" name="passwordConfirm" v-model="user.passwordConfirm" :class="userErrors.passwordConfirm ? 'invalid' : ''" minlength="4" required v-validate />
       <div class="error">{{ validationErrors.passwordConfirm }} {{ userErrors.passwordConfirm }}</div>
