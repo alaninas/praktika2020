b5dcaa31b314bf8a1725f5cf0c3c196638615a81
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-13 09:49:21 +0200

    Add redirectWithParam

--- nestMongoVue-user-app/user-app-frontend/src/modules/utilities/router-utility.ts
+++ nestMongoVue-user-app/user-app-frontend/src/modules/utilities/router-utility.ts
@@ -4,6 +4,11 @@ function routerRedirect (to: string) {
   router.push({ name: to })
 }
 
+function routerRedirectWId (to: string, id: string) {
+  router.push({ name: to, params: { id } })
+}
+
 export {
-  routerRedirect
+  routerRedirect,
+  routerRedirectWId
 }
