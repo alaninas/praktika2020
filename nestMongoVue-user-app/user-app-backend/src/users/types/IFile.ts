d3cbc7ab05f14a54f085078ef3d0761a12982be0
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-16 00:22:22 +0200

    Fix file typings

--- nestMongoVue-user-app/user-app-backend/src/users/types/IFile.ts
+++ nestMongoVue-user-app/user-app-backend/src/users/types/IFile.ts
@@ -1,5 +1,5 @@
 export default interface IFile { 
-    originalname: any; 
-    filename: any; 
-    path: any
+    originalname: string; 
+    filename: string; 
+    path: string;
 }
--- nestMongoVue-user-app/user-app-backend/src/users/utilities/files-interceptor.utility.ts
+++ nestMongoVue-user-app/user-app-backend/src/users/utilities/files-interceptor.utility.ts
@@ -1,15 +1,16 @@
 import { extname } from 'path';
 import * as fs from 'fs';
 import { HttpException, HttpStatus } from '@nestjs/common';
+import IFile from '../types/IFile';
 
-const imageFileFilter = (req, file, callback) => {
+const imageFileFilter = (_req: any, file: IFile, callback: (arg0: HttpException, arg1: boolean) => void) => {
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
     return callback(new HttpException('Only image files are allowed!', HttpStatus.BAD_REQUEST), false);
   }
   callback(null, true);
 };
 
-const editFileName = (req, file, callback) => {
+const editFileName = (_req: any, file: IFile, callback: (arg0: any, arg1: string) => void) => {
   const name = file.originalname.split('.')[0];
   const fileExtName = extname(file.originalname);
   const randomName = Array(4)
@@ -19,7 +20,7 @@ const editFileName = (req, file, callback) => {
   callback(null, `${name}-${randomName}${fileExtName}`);
 };
 
-const editDestination = (req, file, callback) => {
+const editDestination = (req: { params: { id: string; }; }, _file: IFile, callback: (arg0: HttpException, arg1: string | boolean) => void) => {
   const path = `${process.env.MULTER_OPTIONS_DESTINATION}/${req.params.id}`;
   let stat = null;
   try {
@@ -28,7 +29,7 @@ const editDestination = (req, file, callback) => {
     fs.mkdirSync(path, { recursive: true });
   }
   if (stat && !stat.isDirectory()) {
-    callback(new HttpException(`Directory cannot be created because an inode of a different type exists at: ${path}`, HttpStatus.INTERNAL_SERVER_ERROR));
+    callback(new HttpException(`Directory cannot be created because an inode of a different type exists at: ${path}`, HttpStatus.INTERNAL_SERVER_ERROR), false);
   } 
   callback(null, path);
 };
