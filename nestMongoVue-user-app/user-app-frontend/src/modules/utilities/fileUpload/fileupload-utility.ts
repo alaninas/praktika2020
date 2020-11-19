26d05445e0de83c6024a15cd9291314f815e6cd6
Alina Belova <alina.belova@mif.stud.vu.lt>
2020-11-20 00:55:44 +0200

    Add utility functions

--- nestMongoVue-user-app/user-app-frontend/src/modules/utilities/fileUpload/fileupload-utility.ts
+++ nestMongoVue-user-app/user-app-frontend/src/modules/utilities/fileUpload/fileupload-utility.ts
@@ -1,8 +1,28 @@
+import { FileErrorsInterface } from '@/modules/types/IErors'
+import { UploadFileInterface } from '@/modules/types/IUploadFile'
+
 const acceptedFormats = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+// const acceptedFormats = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif', 'text/plain']
 
-const sizeLimit = 1000
+const sizeLimit = 1000000
 
-const fileCountLimit = 20
+const fileCountLimit = 3
+// const fileCountLimit = 20
+
+const getFileErrorString = {
+  httpresponse (err: FileErrorsInterface, progress: number): string {
+    const length = err.httpresponse?.length
+    return length ? ` Response: ${err.httpresponse}. Upload progress: ${progress}` : ''
+  },
+  size (err: FileErrorsInterface): string {
+    const length = err.size?.length
+    return length ? ` File size: ${err.size}` : ''
+  },
+  format (err: FileErrorsInterface): string {
+    const length = err.format?.length
+    return length ? ` File format: ${err.format}` : ''
+  }
+}
 
 function isFormatAcceptable (type: string) {
   return acceptedFormats.indexOf(type) > -1
@@ -16,8 +36,31 @@ function isFileCountAcceptable (count: number) {
   return count <= fileCountLimit
 }
 
+function createFormData ({ inputImage, caption }: { inputImage: File; caption: string }): FormData {
+  const formData = new FormData()
+  formData.append('images', inputImage)
+  formData.append('imagecaption', caption)
+  return formData
+}
+
+function createFileInputErrors (file: File): FileErrorsInterface {
+  const errors: FileErrorsInterface = { format: '', size: '' } as FileErrorsInterface
+  if (!isSizeAcceptable(file.size)) errors.size = `${file.size} exceeds the limit ${sizeLimit}.`
+  if (!isFormatAcceptable(file.type)) errors.format = `${file.type} is not supported. Currently supporting: ${acceptedFormats.toString()}.`
+  return errors
+}
+
+function doesFileHaveInputErrors (file: UploadFileInterface): boolean {
+  return !!(file.errors && (file.errors.size || file.errors.format))
+}
+
 export {
   isFormatAcceptable,
   isSizeAcceptable,
-  isFileCountAcceptable
+  isFileCountAcceptable,
+  createFormData,
+  createFileInputErrors,
+  doesFileHaveInputErrors,
+  fileCountLimit,
+  getFileErrorString
 }
